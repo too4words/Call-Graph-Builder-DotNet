a569f6deb9deb3a07776135172ca74810663e3ad
garbervetsky <diego.garbervetsky@gmail.com>
2015-08-10 07:57:26 -0700

    Trying to run tests in azure

--- WebRole1/AnalysisClient.cs
+++ WebRole1/AnalysisClient.cs
@@ -23,5 +23,57 @@ namespace WebRole1
             var callgraph = await analyzer.AnalyzeAsync(AnalysisStrategyKind.ONDEMAND_ORLEANS);
             return callgraph;
         }
+
+        public async Task<string> RunSingleTestAsync(string testFullName, int iterations)
+        {
+            var index = testFullName.LastIndexOf('.');
+            var testClass = testFullName.Substring(0, index);
+            var testMethod = testFullName.Substring(index + 1);
+
+            return await this.RunSingleTestAsync(testClass, testMethod, iterations);
+        }
+
+        public async Task<string> RunSingleTestAsync(string testClass, string testMethod, int iterations)
+        {
+            try
+            {
+                Console.WriteLine("Executing {0}", testMethod);
+                var minTime = long.MaxValue;
+                var maxTime = 0L;
+                var acumTime = 0D;
+
+                for (var i = 0; i < iterations; i++)
+                {
+                    Console.WriteLine("Iteration {0}", i);
+                    //var watch = new Stopwatch();
+                    var testType = Type.GetType(testClass + ", ReachingTypeAnalysis");
+                    var test = Activator.CreateInstance(testType);
+                    var methodToExecute = test.GetType().GetMethod(testMethod);
+
+                    //watch.Start();
+                    methodToExecute.Invoke(test, new object[1] {AnalysisStrategyKind.ONDEMAND_ORLEANS });
+                    //watch.Stop();
+
+                    //var time = watch.ElapsedMilliseconds;
+                    //if (time > maxTime) maxTime = time;
+                    //if (time < minTime) minTime = time;
+
+                    //acumTime += time;
+                }
+
+                //var avgTime = acumTime / iterations;
+                //outputWriter.WriteLine("{3}, {0}, {1}, {2} {4]", avgTime, maxTime, minTime, testMethod, SolutionAnalyzer.MessageCounter);
+                //outputWriter.Flush();
+                return "Done";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return "Done";
+
+        }
+
+
     }
 }
--- WebRole1/Default.aspx
+++ WebRole1/Default.aspx
@@ -6,15 +6,15 @@
 
     <div class="jumbotron">
         <h1>
-			<asp:Button ID="Button2" runat="server" Text="Button" />
 			CG Azure Starting Point</h1>
         <p class="lead">Trying to run the Silos</p>
         <p>
-            <asp:TextBox ID="TextBox1" runat="server" Height="341px" Width="670px" TextMode="MultiLine"></asp:TextBox>
+            <asp:TextBox ID="TextBox1" runat="server" Height="341px" Width="670px" TextMode="MultiLine" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
         </p>
         <p>
             <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Test Solution" />
         	<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="TestSource" />
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Test Test" />
         </p>
 		<p>
             <asp:TextBox ID="TextBoxPathPrefix" runat="server" Width="675px">c:\Temp\solutions</asp:TextBox>
