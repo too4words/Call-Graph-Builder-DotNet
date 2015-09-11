8dcc2d17bab914d3a99fca67344d87c31737476e
garbervetsky <tiara20>
2015-09-11 03:42:10 -0700

    Merge conflicts and add one script

---
+++ AzureScripts/InvokeExperiment.ps1
@@ -0,0 +1,7 @@
+﻿#
+#
+$resource = "http://orleansservice.cloudapp.net/"
+$controler="api/Orleans"
+$cmd = "?testName=LongTest1&machines=2&numberOfMethods=10"
+$uri = $resource+$controler+$cmd
+Invoke-WebRequest -Uri $uri 
