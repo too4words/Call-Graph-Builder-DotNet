78ea0ae6ea83f5fe3217a5ebf9573b6b46844b01
garbervetsky <tiara20>
2015-09-11 02:16:45 -0700

    slight change in the config for azure webapi

---
+++ TestsSolutions/LongTest2/LongTest2.cs
@@ -0,0 +1,995 @@
+class C
+{
+    public static void N0()
+    {
+        N19();
+        N47();
+        N81();
+        N52();
+        N77();
+        N39();
+    }
+
+    public static void N1()
+    {
+        N40();
+        N25();
+        N14();
+        N52();
+    }
+
+    public static void N2()
+    {
+        N78();
+        N56();
+        N8();
+    }
+
+    public static void N3()
+    {
+        N45();
+        N83();
+        N60();
+        N80();
+        N50();
+        N94();
+    }
+
+    public static void N4()
+    {
+        N90();
+        N96();
+        N44();
+        N8();
+    }
+
+    public static void N5()
+    {
+        N72();
+        N39();
+        N21();
+        N93();
+        N0();
+    }
+
+    public static void N6()
+    {
+        N73();
+        N54();
+        N28();
+        N40();
+        N30();
+        N7();
+        N65();
+    }
+
+    public static void N7()
+    {
+        N83();
+        N84();
+        N18();
+    }
+
+    public static void N8()
+    {
+        N63();
+        N12();
+        N30();
+        N88();
+        N45();
+        N61();
+        N58();
+    }
+
+    public static void N9()
+    {
+        N16();
+        N94();
+        N27();
+        N90();
+        N5();
+    }
+
+    public static void N10()
+    {
+        N87();
+        N60();
+        N24();
+        N67();
+    }
+
+    public static void N11()
+    {
+        N12();
+        N39();
+        N52();
+        N92();
+        N98();
+    }
+
+    public static void N12()
+    {
+        N90();
+        N12();
+        N78();
+        N59();
+        N49();
+        N84();
+        N63();
+    }
+
+    public static void N13()
+    {
+        N52();
+        N83();
+        N40();
+        N44();
+        N23();
+    }
+
+    public static void N14()
+    {
+        N91();
+        N38();
+        N68();
+        N60();
+        N34();
+    }
+
+    public static void N15()
+    {
+        N43();
+        N39();
+        N82();
+        N56();
+        N52();
+        N71();
+    }
+
+    public static void N16()
+    {
+        N88();
+        N50();
+        N7();
+        N34();
+        N60();
+        N18();
+    }
+
+    public static void N17()
+    {
+        N86();
+        N78();
+        N89();
+        N12();
+        N95();
+    }
+
+    public static void N18()
+    {
+        N92();
+        N71();
+        N51();
+    }
+
+    public static void N19()
+    {
+        N55();
+        N65();
+        N29();
+        N88();
+        N64();
+        N43();
+        N21();
+    }
+
+    public static void N20()
+    {
+        N23();
+        N92();
+        N43();
+        N7();
+        N9();
+        N62();
+    }
+
+    public static void N21()
+    {
+        N26();
+        N40();
+        N87();
+    }
+
+    public static void N22()
+    {
+        N30();
+        N1();
+        N20();
+        N85();
+    }
+
+    public static void N23()
+    {
+        N20();
+        N1();
+        N14();
+        N94();
+    }
+
+    public static void N24()
+    {
+        N15();
+        N70();
+        N77();
+        N74();
+        N13();
+        N62();
+        N76();
+        N67();
+        N17();
+        N63();
+        N36();
+    }
+
+    public static void N25()
+    {
+        N90();
+        N86();
+        N96();
+        N19();
+        N91();
+    }
+
+    public static void N26()
+    {
+        N47();
+        N79();
+        N59();
+        N45();
+        N49();
+        N0();
+    }
+
+    public static void N27()
+    {
+        N2();
+        N70();
+        N32();
+        N55();
+        N36();
+        N73();
+        N98();
+        N50();
+        N59();
+        N14();
+    }
+
+    public static void N28()
+    {
+        N29();
+        N1();
+        N81();
+        N57();
+    }
+
+    public static void N29()
+    {
+        N90();
+        N58();
+        N51();
+    }
+
+    public static void N30()
+    {
+        N92();
+        N35();
+        N46();
+    }
+
+    public static void N31()
+    {
+        N53();
+        N71();
+        N7();
+    }
+
+    public static void N32()
+    {
+        N79();
+        N7();
+        N80();
+    }
+
+    public static void N33()
+    {
+        N83();
+        N10();
+        N41();
+    }
+
+    public static void N34()
+    {
+        N69();
+        N11();
+        N43();
+        N68();
+        N20();
+        N57();
+        N8();
+        N64();
+        N25();
+        N26();
+        N21();
+    }
+
+    public static void N35()
+    {
+        N9();
+        N61();
+        N19();
+        N32();
+        N51();
+        N67();
+    }
+
+    public static void N36()
+    {
+        N42();
+        N8();
+        N0();
+        N39();
+        N56();
+        N71();
+        N77();
+        N16();
+    }
+
+    public static void N37()
+    {
+        N15();
+        N5();
+        N51();
+        N25();
+        N90();
+    }
+
+    public static void N38()
+    {
+        N49();
+        N5();
+    }
+
+    public static void N39()
+    {
+        N14();
+        N5();
+        N53();
+        N2();
+    }
+
+    public static void N40()
+    {
+        N31();
+        N44();
+        N72();
+        N65();
+        N15();
+    }
+
+    public static void N41()
+    {
+        N49();
+        N31();
+        N19();
+    }
+
+    public static void N42()
+    {
+        N92();
+        N31();
+        N56();
+    }
+
+    public static void N43()
+    {
+        N91();
+        N19();
+        N4();
+        N25();
+    }
+
+    public static void N44()
+    {
+        N66();
+        N43();
+        N12();
+        N76();
+        N6();
+        N38();
+        N5();
+        N19();
+    }
+
+    public static void N45()
+    {
+        N72();
+        N89();
+        N70();
+        N15();
+        N85();
+        N74();
+        N21();
+        N84();
+        N33();
+        N59();
+    }
+
+    public static void N46()
+    {
+        N54();
+        N85();
+        N66();
+        N78();
+        N53();
+    }
+
+    public static void N47()
+    {
+        N19();
+        N64();
+        N94();
+        N8();
+        N82();
+        N56();
+    }
+
+    public static void N48()
+    {
+        N11();
+        N52();
+        N17();
+        N39();
+    }
+
+    public static void N49()
+    {
+        N84();
+        N65();
+        N1();
+        N28();
+        N35();
+    }
+
+    public static void N50()
+    {
+        N25();
+        N2();
+    }
+
+    public static void N51()
+    {
+        N50();
+        N64();
+        N56();
+        N78();
+        N20();
+        N12();
+        N47();
+        N86();
+    }
+
+    public static void N52()
+    {
+        N67();
+        N63();
+        N77();
+        N11();
+        N35();
+        N58();
+    }
+
+    public static void N53()
+    {
+        N92();
+        N76();
+        N62();
+        N89();
+    }
+
+    public static void N54()
+    {
+        N77();
+        N94();
+        N57();
+        N86();
+        N2();
+        N37();
+    }
+
+    public static void N55()
+    {
+        N98();
+        N4();
+        N79();
+        N18();
+    }
+
+    public static void N56()
+    {
+        N52();
+    }
+
+    public static void N57()
+    {
+        N1();
+        N50();
+        N64();
+        N42();
+        N2();
+        N19();
+    }
+
+    public static void N58()
+    {
+        N27();
+        N49();
+        N87();
+        N24();
+        N42();
+        N64();
+    }
+
+    public static void N59()
+    {
+        N40();
+        N64();
+        N63();
+        N95();
+        N2();
+        N12();
+    }
+
+    public static void N60()
+    {
+        N85();
+        N3();
+        N48();
+        N1();
+    }
+
+    public static void N61()
+    {
+        N22();
+        N4();
+        N8();
+        N38();
+        N39();
+        N84();
+        N56();
+    }
+
+    public static void N62()
+    {
+        N61();
+        N88();
+        N24();
+        N15();
+    }
+
+    public static void N63()
+    {
+        N45();
+        N21();
+        N24();
+        N4();
+        N7();
+    }
+
+    public static void N64()
+    {
+        N35();
+        N12();
+        N39();
+        N6();
+        N31();
+    }
+
+    public static void N65()
+    {
+        N27();
+        N6();
+        N16();
+        N81();
+        N13();
+    }
+
+    public static void N66()
+    {
+        N83();
+        N16();
+        N49();
+        N55();
+        N27();
+        N96();
+    }
+
+    public static void N67()
+    {
+        N2();
+        N78();
+        N51();
+        N67();
+    }
+
+    public static void N68()
+    {
+    }
+
+    public static void N69()
+    {
+        N39();
+        N21();
+        N23();
+    }
+
+    public static void N70()
+    {
+        N49();
+        N58();
+        N98();
+        N77();
+        N32();
+    }
+
+    public static void N71()
+    {
+        N55();
+        N48();
+    }
+
+    public static void N72()
+    {
+        N18();
+        N9();
+    }
+
+    public static void N73()
+    {
+        N34();
+        N45();
+        N20();
+        N73();
+    }
+
+    public static void N74()
+    {
+        N94();
+        N18();
+        N69();
+        N23();
+    }
+
+    public static void N75()
+    {
+        N16();
+        N87();
+        N44();
+    }
+
+    public static void N76()
+    {
+        N97();
+        N50();
+        N35();
+        N60();
+        N28();
+    }
+
+    public static void N77()
+    {
+        N65();
+        N10();
+        N64();
+        N19();
+        N86();
+    }
+
+    public static void N78()
+    {
+        N44();
+        N23();
+        N81();
+        N34();
+        N79();
+        N74();
+        N72();
+        N16();
+    }
+
+    public static void N79()
+    {
+        N35();
+        N37();
+        N49();
+        N98();
+        N94();
+    }
+
+    public static void N80()
+    {
+        N45();
+        N19();
+        N74();
+        N97();
+        N91();
+        N98();
+    }
+
+    public static void N81()
+    {
+        N16();
+        N38();
+        N44();
+        N7();
+        N37();
+    }
+
+    public static void N82()
+    {
+        N90();
+        N75();
+        N7();
+        N92();
+        N45();
+    }
+
+    public static void N83()
+    {
+        N70();
+        N22();
+        N92();
+    }
+
+    public static void N84()
+    {
+        N40();
+        N66();
+        N24();
+        N67();
+        N39();
+        N33();
+        N16();
+        N50();
+        N78();
+        N30();
+        N52();
+    }
+
+    public static void N85()
+    {
+        N0();
+        N60();
+        N15();
+        N44();
+    }
+
+    public static void N86()
+    {
+        N1();
+        N39();
+        N41();
+        N53();
+    }
+
+    public static void N87()
+    {
+        N84();
+        N63();
+        N58();
+    }
+
+    public static void N88()
+    {
+        N48();
+        N72();
+        N18();
+        N37();
+        N12();
+    }
+
+    public static void N89()
+    {
+        N98();
+        N37();
+        N59();
+        N14();
+        N81();
+        N2();
+        N51();
+    }
+
+    public static void N90()
+    {
+        N32();
+        N14();
+        N45();
+        N15();
+    }
+
+    public static void N91()
+    {
+        N89();
+        N67();
+        N74();
+        N27();
+        N39();
+    }
+
+    public static void N92()
+    {
+        N6();
+        N49();
+        N22();
+        N29();
+        N95();
+        N13();
+        N85();
+    }
+
+    public static void N93()
+    {
+        N4();
+        N41();
+        N47();
+        N93();
+    }
+
+    public static void N94()
+    {
+        N22();
+    }
+
+    public static void N95()
+    {
+        N80();
+        N89();
+        N45();
+        N94();
+        N2();
+        N4();
+    }
+
+    public static void N96()
+    {
+        N79();
+        N64();
+        N78();
+        N98();
+        N29();
+    }
+
+    public static void N97()
+    {
+        N15();
+        N64();
+        N90();
+    }
+
+    public static void N98()
+    {
+        N81();
+        N4();
+        N61();
+        N20();
+        N70();
+    }
+
+    public static void N99()
+    {
+    }
+
+    public static void Main()
+    {
+        N0();
+        N1();
+        N2();
+        N3();
+        N4();
+        N5();
+        N6();
+        N7();
+        N8();
+        N9();
+        N10();
+        N11();
+        N12();
+        N13();
+        N14();
+        N15();
+        N16();
+        N17();
+        N18();
+        N19();
+        N20();
+        N21();
+        N22();
+        N23();
+        N24();
+        N25();
+        N26();
+        N27();
+        N28();
+        N29();
+        N30();
+        N31();
+        N32();
+        N33();
+        N34();
+        N35();
+        N36();
+        N37();
+        N38();
+        N39();
+        N40();
+        N41();
+        N42();
+        N43();
+        N44();
+        N45();
+        N46();
+        N47();
+        N48();
+        N49();
+        N50();
+        N51();
+        N52();
+        N53();
+        N54();
+        N55();
+        N56();
+        N57();
+        N58();
+        N59();
+        N60();
+        N61();
+        N62();
+        N63();
+        N64();
+        N65();
+        N66();
+        N67();
+        N68();
+        N69();
+        N70();
+        N71();
+        N72();
+        N73();
+        N74();
+        N75();
+        N76();
+        N77();
+        N78();
+        N79();
+        N80();
+        N81();
+        N82();
+        N83();
+        N84();
+        N85();
+        N86();
+        N87();
+        N88();
+        N89();
+        N90();
+        N91();
+        N92();
+        N93();
+        N94();
+        N95();
+        N96();
+        N97();
+        N98();
+        N99();
+        Main();
+    }
+}
